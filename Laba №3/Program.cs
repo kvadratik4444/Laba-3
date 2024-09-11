4beedca37a83b783ce68a706f1a907cc7fe9df01
kvadratik4444 <154566855+kvadratik4444@users.noreply.github.com>
2024-09-11 20:37:01 +0700

    Update Program.cs

--- Laba №3/Program.cs
+++ Laba №3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +10,9 @@ namespace Laba__3
     {
         public static void F1()
         {
-            int x;
+            int x, i;
             var rand = new Random();
             int ans = rand.Next(1, 11);
-            int i;
             Console.WriteLine("Введите число");
             for (i = 1; ;i++)
             {
@@ -26,19 +25,67 @@ namespace Laba__3
         }
         private static void F2()
         {
-
+            int sum = 0, SumSquare = 0;
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1;i <= N; i++)
+            {
+                sum += i;
+                SumSquare += i * i;
+                if (SumSquare > 500) break;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(SumSquare);
         }
         private static void F3()
         {
-
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Максимальное кол-во подтягиваний в группе {N * 16}");
+            Console.WriteLine($"Минимальное кол-во подтягиваний в группе {N * 12}");
         }
         private static void F4()
         {
-
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            double C = Convert.ToDouble(Console.ReadLine());
+            double D = Convert.ToDouble(Console.ReadLine());
+            double sum = 0, y, AverageY = 0;
+            for (int x = 1;x <= 10; x++)
+            {
+                y = A * Math.Sqrt(B * x + D) - C * x;
+                if (y > 0) sum += y;
+                AverageY += y;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(AverageY/10);
         }
         private static void F5()
         {
-
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+            int MaxEl = arr[0], MaxI = 0;
+            for (int i = 1; i < N; i++)
+            {
+                if (arr[i] >= MaxEl)
+                { 
+                    MaxEl = arr[i];
+                    MaxI = i;
+                }
+            }
+            Console.WriteLine(MaxEl);
+            Console.WriteLine(MaxI);
+        }
+        private static void F6()
+        {
+            double R = Convert.ToDouble(Console.ReadLine());
+            int ans = 0;
+            for (int i = 0; i < 3; i++) 
+            { 
+                double x = Convert.ToDouble(Console.ReadLine());
+                double y = Convert.ToDouble(Console.ReadLine());
+                if (R * R * 4 >= x * x + y * y) ans++;
+            }
+            Console.WriteLine(ans);
         }
         static void Main(string[] args)
         {
@@ -57,6 +104,8 @@ namespace Laba__3
                     F4(); break;
                 case 5:
                     F5(); break;
+                case 6:
+                    F6(); break;
             }
         }
     }
